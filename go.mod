42fd91882852d6c724bfe41ae18ff4dbfda5c64c
Alex <alex.streeter@airlockdigital.com>
2023-10-31 00:45:49 +1030

    updated to ignore dist folder

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module mfrp
 
 go 1.20
+
+require github.com/AlexTheProgrammer/sail v0.0.0-20231030135755-77227b9545bc
+
+replace github.com/AlexTheProgrammer/sail => ../sail
+
--- main.go
+++ main.go
@@ -1,21 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"syscall/js"
+	"github.com/AlexTheProgrammer/sail/dom"
 )
-
-var htmlString = "<h2>Nothing like some html in a string</h2>"
-
-func GetHtml() js.Func {
-	return js.FuncOf(func(this js.Value, args []js.Value) any {
-		return htmlString
-	})
-}
 
 func main() {
 	ch := make(chan bool)
-	fmt.Printf("call console.log from golang\n")
-	js.Global().Set("getHtml", GetHtml())
+	d := dom.DOM{
+		Body: []dom.Node{
+			&dom.Div{InnerHTML: "Hi there sail and raft"},
+		},
+	}
+	d.Render()
+
 	<-ch // block indefinitely to keep program alive
 }
