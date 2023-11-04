19442eb9e29bc4f0e0587490fa431376fba7f4bf
Alex <alex.streeter@airlockdigital.com>
2023-11-04 18:44:43 +1030

    adding in tailwind direct

--- main.go
+++ main.go
@@ -24,10 +24,19 @@ func main() {
 					d.UL(d.Props{Class: d.NewClass("space-y-2 font-medium")},
 						d.LI(d.Props{},
 							d.A(
-								d.Props{Class: d.NewClass("flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group")},
+								d.Props{
+									Class: d.NewClass(
+										"flex items-center",                              // center items in menu
+										"p-2 text-gray-900 rounded-lg dark:text-white",   // text formatting
+										"hover:bg-gray-100 dark:hover:bg-gray-700 group", // hover behaviour
+									),
+								},
 								d.SVG(
 									d.Props{
-										Class:      d.NewClass("w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"),
+										Class: d.NewClass(
+											"w-5 h-5 text-gray-500 dark:text-gray-400",                                     // sizing
+											"transition duration-75 group-hover:text-gray-900 dark:group-hover:text-white", // hover effects
+										),
 										AriaHidden: "true",
 										XMLNS:      "http://www.w3.org/2000/svg",
 										Fill:       "currentColor",
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ["./**/*.{html,js,go}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
